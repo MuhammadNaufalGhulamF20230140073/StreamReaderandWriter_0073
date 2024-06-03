1ad640ff911bb69c60c073498f2543a432146c70
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2024-06-03 14:55:57 +0700

    membuka file dalam mode menulis, dan membuat nama file dari format.txt yang digunakan untuk menyimpan tulisan, untuk selesai menulis tekan Q

--- iofilewithpath.cpp
+++ iofilewithpath.cpp
@@ -10,7 +10,10 @@ int main(){
     cout << "Masukan Nama File : ";
     cin >> NamaFile;
 
+    //membuka file mode menulis
+    ofstream outfile;
+    //menunjuk nama file
+    outfile.open(NamaFile + ".txt", ios::out);
 
+    cout << ">= Menulis File, \'q\' untuk keluar" << endl;
 
-
-}
