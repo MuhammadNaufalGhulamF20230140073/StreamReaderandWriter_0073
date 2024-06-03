1bdc029ee4a572b7fe8581d9ef28752b89f151e2
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2024-06-03 14:52:40 +0700

    membuat string baris,Nama File . dan membuat masukan ke 'NamFile'

--- iofilewithpath.cpp
+++ iofilewithpath.cpp
@@ -10,18 +10,7 @@ int main(){
     cout << "Masukan Nama File : ";
     cin >> NamaFile;
 
-    //membuka file mode menulis
-    ofstream outfile;
-    //menunjuk nama file
-    outfile.open(NamaFile + ".txt", ios::out);
 
-    cout << ">= Menulis File, \'q\' untuk keluar" << endl;
-
-    //unlimited loop untuk menulis
-    while(true){
-        cout << "-";
-        //mendapatkan setiap karakter dalam satu baris
-    }
 
 
 }
