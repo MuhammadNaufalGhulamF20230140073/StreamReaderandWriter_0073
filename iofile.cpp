eb41e8b9844c8d1f5fbed046f29190e969356288
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2024-06-03 14:45:47 +0700

    menuju sebuah file untuk dibuka jika ada nama filetersebut maka di buka dan di tampilkan baris

--- iofile.cpp
+++ iofile.cpp
@@ -22,3 +22,24 @@ int main(){
         //menulis dan memasukan nilai dari 'baris' ke dalam file
         outfile << baris << endl;
     }
+    //selesai dalam menulis sekarang tutup file
+    outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+    //menunjuk sebuah file
+    infile.open("contohfile.txt");
+
+    cout << endl << ">= Membuka dan Membaca File" << endl;
+    //jika fike ada maka buka
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while(getline(infile, baris))
+        {
+            //tampilkan 
+            cout << baris <<'\n';
+        }
+
+        //tutup file setelah selesai
+        infile.close();
