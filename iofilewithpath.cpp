21353f10142d1dd475e082eaf75645355221c1f3
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2024-06-03 14:44:26 +0700

    untuk menutup file

---
+++ iofilewithpath.cpp
@@ -0,0 +1,27 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string baris;
+    string NamaFile;
+
+    cout << "Masukan Nama File : ";
+    cin >> NamaFile;
+
+    //membuka file mode menulis
+    ofstream outfile;
+    //menunjuk nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis File, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis
+    while(true){
+        cout << "-";
+        //mendapatkan setiap karakter dalam satu baris
+    }
+
+
+}
